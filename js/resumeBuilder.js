2e7aead12f63a96dd4e1632ad8452e5d5965da7e
Kamry <kamdugle@gmail.com>
2016-02-01 21:02:40 -0700

    Create first four JSON objects.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,4 @@
-var skills = ["HTML, CSS, Javascript"]
+/* var skills = ["HTML, CSS, Javascript"]
 
 var bio = {
 	name: "Kam Bowman",
@@ -20,4 +20,67 @@ var formattedBioPic = HTMLbioPic.replace("%data%", bio.bioPic);
 var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMsg);
 var formattedSkills = HTMLskills.replace("%data%", bio.skills);
 
-$("#main").prepend(formattedBioPic, formattedWelcomeMsg, formattedSkills, formattedEmail);
+$("#main").prepend(formattedBioPic, formattedWelcomeMsg, formattedSkills, formattedEmail); */
+
+var "work" = {
+	"jobs": [
+	{
+		"employer": "Colorado Lending Source",
+		"title": "Loan Officer",
+		"years": "2013-2016",
+		"location": "Denver, Colorado",
+		"description": "Underwrote SBA 504 loan packages and managed SBA approval process"	
+	},
+	{
+		"employer": "Clear Creek Consulting",
+		"title": "Junior Associate",
+		"years": "2012-2013",
+		"location": "Denver, Colorado",
+		"description": "Worked on portfolio of 40 clients, resolving tax lien and collection issues on their behalf with IRS and state governments"		
+	},
+	{
+		"employer": "Ministry of Education, Spain",
+		"title": "Language Assistant",
+		"years": "2010-2012",
+		"location": "Madrid, Spain",
+		"description": "Participated in language immersion programs in elementary and high schools in Spain"		
+	}
+	]};
+
+}
+	work.title = "Loan Officer";
+	work.employer = "Colorado Lending Source";
+	work.dates = "2013-Present"
+
+var education = {
+	"schools": [
+		{"name": "University of Michigan",
+		"city": "Ann Arbor, Michigan",
+		"majors": "Philosophy, Economics",
+		"years": "2008-2010"
+		},
+		{"name": "Western Michigan University",
+		"city": "Kalamazoo, Michigan",
+		"majors": "Philosophy, Economics",
+		"years": "2006-2008"
+		}]
+};
+
+var projects = {[
+	{
+		"title": "Mock website",
+		"dates": "2016",
+		"description": "Made various websites on Udacity website.",
+		"images" = "https://www.tedcruz.org/wp-content/uploads/2016/01/20160128_RockyRubio.png"
+	}]
+};
+
+var bio = {
+	"name": "Kam Bowman",
+	"role": "Front End Web Developer",
+	"welcomeMessage": "Get stuffed, kid, haha!",
+	"contacts": {
+		"mobile": "555-555-5555",
+		"email": "Kamry.Bowman@gmail.com",
+		"github": "Kamdugle"}
+};
